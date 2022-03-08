170656b8c035e55333537643aff88a6dace9643d
JoseBieco <jo_bieco@hotmail.com>
2022-03-08 16:48:15 -0300

    docs: document application start point

--- src/main/kotlin/api/atomical/AtomicalApplication.kt
+++ src/main/kotlin/api/atomical/AtomicalApplication.kt
@@ -6,6 +6,9 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class AtomicalApplication
 
+/**
+ * Application start point
+ */
 fun main(args: Array<String>) {
 	runApplication<AtomicalApplication>(*args)
 }

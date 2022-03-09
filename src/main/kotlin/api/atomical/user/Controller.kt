fe72c13b6d0cce31015ae780f201341244c1aa52
JoseBieco <jo_bieco@hotmail.com>
2022-03-09 15:26:20 -0300

    docs: functions of user controller

--- src/main/kotlin/api/atomical/user/Controller.kt
+++ src/main/kotlin/api/atomical/user/Controller.kt
@@ -1,4 +1,15 @@
 package api.atomical.user
 
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("user")
 class Controller {
+    /**
+     * Get by Id
+     * Get All
+     * Update
+     * Delete
+     */
 }

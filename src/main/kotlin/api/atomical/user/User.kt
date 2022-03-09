7cd4c7491d80892f89bda7e010c2e05c5f99bf98
José Eduardo Saroba Bieco <josebieco@pop-os.localdomain>
2022-03-09 19:52:22 -0300

    feat(user/User): add constructor to RegisterDto

--- src/main/kotlin/api/atomical/user/User.kt
+++ src/main/kotlin/api/atomical/user/User.kt
@@ -1,8 +1,12 @@
 package api.atomical.user
 
+import api.atomical.auth.dto.RegisterDto
 import com.fasterxml.jackson.annotation.JsonIgnore
 import javax.persistence.*
 
+/**
+ * Just used to represent the entity
+ */
 @Entity
 @Table(name = "user")
 class User (
@@ -20,4 +24,12 @@ class User (
     @Column(name= "password", nullable = false)
     @JsonIgnore
     var password: String? = null
-)
+){
+    constructor(user: RegisterDto): this() {
+        this.apply {
+            name = user.name
+            email = user.email
+            password = user.password
+        }
+    }
+}

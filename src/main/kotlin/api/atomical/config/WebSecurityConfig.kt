e8f92d545e294dbb3202e1e2949e0eba94e90ad3
JoseBieco <jo_bieco@hotmail.com>
2022-03-09 15:43:19 -0300

    feat(config/WebSecurityConfig): set the encoder bean

--- src/main/kotlin/api/atomical/config/WebSecurityConfig.kt
+++ src/main/kotlin/api/atomical/config/WebSecurityConfig.kt
@@ -1,4 +1,18 @@
 package api.atomical.config
 
-class WebSecurityConfig {
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@EnableWebSecurity
+class WebSecurityConfig: WebSecurityConfigurerAdapter() {
+    /**
+     * Set the encoder default bcrypt version
+     */
+    @Bean
+    fun encoder(): PasswordEncoder {
+        return BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.`$2A`)
+    }
 }

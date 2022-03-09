633fe1c9f1d1726e3396ca2e286f92627c096747
JoseBieco <jo_bieco@hotmail.com>
2022-03-09 15:09:23 -0300

    feat(user/dto/Login): created user login dto

--- src/main/kotlin/api/atomical/user/dto/LoginDto.kt
+++ src/main/kotlin/api/atomical/user/dto/LoginDto.kt
@@ -1,4 +1,19 @@
 package api.atomical.user.dto
 
-class LoginDto {
-}
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
+class LoginDto (
+    @NotBlank(message = "Email field cannot be blank")
+    @NotNull(message = "Email field cannot be null.")
+    @Min(value = 2, message = "The email size can't be lower than 2 characters.")
+    var email: String,
+
+    @NotBlank(message = "Password field cannot be blank")
+    @NotNull(message = "Password field cannot be null.")
+    @Min(value = 2, message = "The password size can't be lower than 2 characters.")
+    @Max(value = 18, message = "The password size can't be bigger than 18 characters.")
+    var password: String
+)

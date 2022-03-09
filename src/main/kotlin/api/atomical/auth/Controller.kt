0e21453d09c72155ed829d80478bf9e264ac9a51
JoseBieco <jo_bieco@hotmail.com>
2022-03-09 15:26:52 -0300

    feat(auth/Controller): docs of auth controller functions

--- src/main/kotlin/api/atomical/auth/Controller.kt
+++ src/main/kotlin/api/atomical/auth/Controller.kt
@@ -1,4 +1,13 @@
-package api.atomical.auth.dto
+package api.atomical.auth
 
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("auth")
 class Controller {
+    /**
+     * Register
+     * Login
+     */
 }

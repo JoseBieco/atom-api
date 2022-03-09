9e680eb33d8dbd7b3005bc5891257a36707d4c0b
JoseBieco <jo_bieco@hotmail.com>
2022-03-09 15:09:42 -0300

    feat(user/dto/Register): created user register dto

--- src/main/kotlin/api/atomical/user/dto/RegisterDto.kt
+++ src/main/kotlin/api/atomical/user/dto/RegisterDto.kt
@@ -1,4 +1,24 @@
 package api.atomical.user.dto
 
-class RegisterDto {
-}
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
+class RegisterDto (
+    @NotBlank(message = "Name field cannot be blank")
+    @NotNull(message = "Name field cannot be null.")
+    @Min(value = 2, message = "The name size can't be lower than 2 characters.")
+    var name: String,
+
+    @NotBlank(message = "Email field cannot be blank")
+    @NotNull(message = "Email field cannot be null.")
+    @Min(value = 2, message = "The email size can't be lower than 2 characters.")
+    var email: String,
+
+    @NotBlank(message = "Password field cannot be blank")
+    @NotNull(message = "Password field cannot be null.")
+    @Min(value = 2, message = "The password size can't be lower than 2 characters.")
+    @Max(value = 18, message = "The password size can't be bigger than 18 characters.")
+    var password: String
+)

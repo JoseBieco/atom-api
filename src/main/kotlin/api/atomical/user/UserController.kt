c02c1d8501cb4fe5b4766b7db676305a8ef4a139
José Eduardo Saroba Bieco <josebieco@pop-os.localdomain>
2022-03-09 18:14:48 -0300

    refactor(auth): changed the controller name to AuthController

--- src/main/kotlin/api/atomical/user/UserController.kt
+++ src/main/kotlin/api/atomical/user/UserController.kt
@@ -1,13 +1,15 @@
-package api.atomical.auth
+package api.atomical.user
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("auth")
-class Controller {
+@RequestMapping("user")
+class UserController {
     /**
-     * Register
-     * Login
+     * Get by Id
+     * Get All
+     * Update
+     * Delete
      */
 }

05e4c92e83ddf43331fd5aaef969bb015759fabb
JoseBieco <jo_bieco@hotmail.com>
2022-03-10 16:17:16 -0300

    refactor(user/UserService): implemented soft delete method vvalidating the user id from request

--- src/main/kotlin/api/atomical/user/UserService.kt
+++ src/main/kotlin/api/atomical/user/UserService.kt
@@ -3,8 +3,11 @@ package api.atomical.user
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
+import org.springframework.http.HttpStatus
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+import java.time.LocalDateTime
 
 @Service
 class UserService(
@@ -20,5 +23,21 @@ class UserService(
      */
     fun getAll(pageable: PageRequest): Page<User> {
         return db.getAll(pageable)
+    }
+
+    /**
+     * Alter user deleted_at attribute
+     * Checks if the userId param exists, if not, throws bad request
+     * @param userId Represents the user unique key
+     */
+    fun softDelete(userId: Long): User {
+       val user = db.findById(userId)
+           .orElseThrow{
+               ResponseStatusException(HttpStatus.NOT_FOUND, "User not found.")
+           }
+
+        user.deleted_at = LocalDateTime.now()
+
+        return db.save(user)
     }
 }

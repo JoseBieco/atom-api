9cc86909f77760f4a624fa4f762edb72fb9e4672
JoseBieco <jo_bieco@hotmail.com>
2022-03-10 10:26:41 -0300

    refactor(auth/dtop): fixed validators

--- src/main/kotlin/api/atomical/auth/dto/LoginDto.kt
+++ src/main/kotlin/api/atomical/auth/dto/LoginDto.kt
@@ -1,19 +1,14 @@
 package api.atomical.auth.dto
 
-import javax.validation.constraints.Max
-import javax.validation.constraints.Min
-import javax.validation.constraints.NotBlank
-import javax.validation.constraints.NotNull
+import javax.validation.constraints.*
 
-class LoginDto (
-    @NotBlank(message = "Email field cannot be blank")
-    @NotNull(message = "Email field cannot be null.")
-    @Min(value = 2, message = "The email size can't be lower than 2 characters.")
+data class LoginDto (
+
+    @field:Email(message = "The email must be valid.")
     var email: String,
 
-    @NotBlank(message = "Password field cannot be blank")
-    @NotNull(message = "Password field cannot be null.")
-    @Min(value = 2, message = "The password size can't be lower than 2 characters.")
-    @Max(value = 18, message = "The password size can't be bigger than 18 characters.")
+    @field:NotBlank(message = "Password field cannot be blank")
+    @field:NotNull(message = "Password field cannot be null.")
+    @field:Size(min = 2, message = "The password size can't be lower than 2 characters.")
     var password: String
 )
--- src/main/kotlin/api/atomical/auth/dto/RegisterDto.kt
+++ src/main/kotlin/api/atomical/auth/dto/RegisterDto.kt
@@ -1,24 +1,21 @@
 package api.atomical.auth.dto
 
-import javax.validation.constraints.Max
-import javax.validation.constraints.Min
+import javax.validation.constraints.Email
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 
-class RegisterDto (
-    @NotBlank(message = "Name field cannot be blank")
-    @NotNull(message = "Name field cannot be null.")
-    @Min(value = 2, message = "The name size can't be lower than 2 characters.")
+data class RegisterDto (
+    @field:NotBlank(message = "Name field cannot be blank")
+    @field:NotNull(message = "Name field cannot be null.")
+    @field:Size(min = 2, message = "The name size can't be lower than 2 characters.")
     var name: String,
 
-    @NotBlank(message = "Email field cannot be blank")
-    @NotNull(message = "Email field cannot be null.")
-    @Min(value = 2, message = "The email size can't be lower than 2 characters.")
+    @field:Email(message = "The email must be valid.")
     var email: String,
 
-    @NotBlank(message = "Password field cannot be blank")
-    @NotNull(message = "Password field cannot be null.")
-    @Min(value = 2, message = "The password size can't be lower than 2 characters.")
-    @Max(value = 18, message = "The password size can't be bigger than 18 characters.")
+    @field:NotBlank(message = "Password field cannot be blank")
+    @field:NotNull(message = "Password field cannot be null.")
+    @field:Size(min = 2, message = "The password size can't be lower than 2 characters.")
     var password: String
 )

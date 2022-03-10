f01968f4729ce95cacf122c535f7300ad5abec9d
JoseBieco <jo_bieco@hotmail.com>
2022-03-10 10:31:31 -0300

    feat(auth/AuthService): implemented the register method

--- src/main/kotlin/api/atomical/auth/AuthService.kt
+++ src/main/kotlin/api/atomical/auth/AuthService.kt
@@ -1,4 +1,39 @@
 package api.atomical.auth
 
-class AuthService {
+import api.atomical.auth.dto.RegisterDto
+import api.atomical.user.User
+import api.atomical.user.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+
+@Service
+class AuthService(
+    @Autowired
+    val db: UserRepository,
+
+    @Autowired
+    val passwordEncoder: PasswordEncoder,
+) {
+    /**
+     * Create new entity
+     * @param user RegisterDto
+     * @return The created User
+     * @throws HttpStatus.BAD_REQUEST This email is already registered
+     */
+    fun create(user: RegisterDto): User {
+        /**
+         * Validate email -> must be unique
+         * throw error if it's not unique
+         */
+        if(db.getByEmail(user.email) !== null) {
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "This email is already registered")
+        }
+
+        val createUser = User(user)
+        createUser.encodePassword(passwordEncoder)
+        return this.db.save(createUser)
+    }
 }

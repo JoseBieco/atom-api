60f90f7db4c10a193d0227cb72a017f318cda995
JoseBieco <jo_bieco@hotmail.com>
2022-03-09 23:52:46 -0300

    feat(config/WebSecurityConfig): disable requets authentication

--- src/main/kotlin/api/atomical/config/WebSecurityConfig.kt
+++ src/main/kotlin/api/atomical/config/WebSecurityConfig.kt
@@ -1,6 +1,7 @@
 package api.atomical.config
 
 import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -14,5 +15,9 @@ class WebSecurityConfig: WebSecurityConfigurerAdapter() {
     @Bean
     fun encoder(): PasswordEncoder {
         return BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.`$2A`)
+    }
+
+    override fun configure(http: HttpSecurity?) {
+        http?.authorizeHttpRequests()?.anyRequest()?.permitAll()
     }
 }

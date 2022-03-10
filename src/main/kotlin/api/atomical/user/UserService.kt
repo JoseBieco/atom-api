2d5da60537657875c5aa338a444f9685739137b0
JoseBieco <jo_bieco@hotmail.com>
2022-03-10 10:32:57 -0300

    feat(user/UserService): Autowired the PasswordEncoder and UserRepository

--- src/main/kotlin/api/atomical/user/UserService.kt
+++ src/main/kotlin/api/atomical/user/UserService.kt
@@ -1,4 +1,16 @@
 package api.atomical.user
 
-class UserService {
+import api.atomical.auth.dto.RegisterDto
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+
+@Service
+class UserService(
+    @Autowired
+    val db: UserRepository,
+
+    @Autowired
+    val passwordEncoder: PasswordEncoder,
+) {
 }

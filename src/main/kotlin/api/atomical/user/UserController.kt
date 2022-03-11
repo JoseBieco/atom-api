0dc2cb2f25b7374d7fa04680d7915207318dbcc3
JoseBieco <jo_bieco@hotmail.com>
2022-03-11 09:16:38 -0300

    refactor(user/UserController): changed the http method of delete, from patch to delete

--- src/main/kotlin/api/atomical/user/UserController.kt
+++ src/main/kotlin/api/atomical/user/UserController.kt
@@ -3,6 +3,7 @@ package api.atomical.user
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -39,7 +40,7 @@ class UserController(
     /**
      * Soft delete user
      */
-    @PatchMapping("/{userId}")
+    @DeleteMapping("/{userId}")
     fun delete(@PathVariable userId: Long): User {
         return service.softDelete(userId)
     }

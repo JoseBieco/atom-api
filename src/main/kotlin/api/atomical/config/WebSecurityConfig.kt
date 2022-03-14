e229224785be91bb7c9409e14ddfbee0618bd429
JoseBieco <jo_bieco@hotmail.com>
2022-03-14 14:38:09 -0300

    feat(config/WebSecurityConfig): configure: routes, authentication manager builder and jwt filter

--- src/main/kotlin/api/atomical/config/WebSecurityConfig.kt
+++ src/main/kotlin/api/atomical/config/WebSecurityConfig.kt
@@ -1,14 +1,29 @@
 package api.atomical.config
 
+import api.atomical.auth.AuthService
+import api.atomical.jwt.JwtAuthFilter
+import api.atomical.jwt.JwtService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 @EnableWebSecurity
 class WebSecurityConfig: WebSecurityConfigurerAdapter() {
+    @Autowired
+    lateinit var userDetailService: UserDetailsServiceImpl
+
+    @Autowired
+    lateinit var jwtService: JwtService
+
     /**
      * Set the encoder default bcrypt version
      */
@@ -17,11 +32,29 @@ class WebSecurityConfig: WebSecurityConfigurerAdapter() {
         return BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.`$2A`)
     }
 
+    /**
+     * Configure the jwt filter
+     */
+    @Bean
+    fun jwtFilter(): OncePerRequestFilter {
+        return JwtAuthFilter(jwtService, userDetailService)
+    }
+    /**
+     * Configure the Service and the encoder method
+     */
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.userDetailsService(userDetailService).passwordEncoder(encoder())
+    }
+
     override fun configure(http: HttpSecurity) {
         http
             .csrf().disable()
             .authorizeHttpRequests()
-            .anyRequest()
-            .permitAll()
+            .antMatchers(HttpMethod.POST, "/auth/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter::class.java)
     }
 }

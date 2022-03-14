a8a02a77efcf295e57c44f660bd147486ab68812
JoseBieco <jo_bieco@hotmail.com>
2022-03-14 14:32:41 -0300

    feat(jwt): Created the service and the filter to authorize requests

--- src/main/kotlin/api/atomical/jwt/JwtAuthFilter.kt
+++ src/main/kotlin/api/atomical/jwt/JwtAuthFilter.kt
@@ -1,4 +1,47 @@
 package api.atomical.jwt
 
-class JwtAuthFilter {
+import api.atomical.auth.AuthService
+import api.atomical.config.UserDetailsServiceImpl
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JwtAuthFilter(
+    @Autowired
+    val jwtService: JwtService,
+
+    @Autowired
+    val userDetailsService: UserDetailsServiceImpl
+): OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+
+        val authorization = if(request.getHeader("Authorization").isNullOrBlank()) null else request.getHeader("Authorization")
+
+        if(!authorization.isNullOrBlank() && authorization.startsWith("Bearer")) {
+            val token = authorization.split(" ")[1]
+            val isValid: Boolean = jwtService.validToken(token)
+
+            if(isValid) {
+                val userDetails: UserDetails = userDetailsService.loadUserByUsername(jwtService.getUserLogin(token))
+
+                val user: UsernamePasswordAuthenticationToken =
+                    UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
+                user.details = (WebAuthenticationDetailsSource().buildDetails(request))
+                SecurityContextHolder.getContext().authentication = user
+            }
+        }
+        filterChain.doFilter(request, response)
+    }
+
 }
--- src/main/kotlin/api/atomical/jwt/JwtService.kt
+++ src/main/kotlin/api/atomical/jwt/JwtService.kt
@@ -1,4 +1,72 @@
 package api.atomical.jwt
 
-class JwtService {
+import api.atomical.user.User
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.Date
+
+@Service
+class JwtService(
+    @Value("\${security.jwt.expiration}")
+    private val expiration: String,
+
+    @Value("\${security.jwt.signatureKey}")
+    private val signatureKey: String,
+) {
+    /**
+     * Generate a token based on user's information
+     * @param user User entity
+     * @return Jwt token
+     */
+    fun generateToken(user: User): String {
+        val expirationTime: LocalDateTime = LocalDateTime.now().plusDays(expiration.toLong())
+        val date = Date.from(expirationTime.atZone(ZoneId.systemDefault()).toInstant())
+
+        return Jwts
+            .builder()
+            .setSubject(user.email)
+            .setExpiration(date)
+            .signWith(SignatureAlgorithm.HS512, signatureKey)
+            .compact()
+    }
+
+    /**
+     * Get information from token
+     * @param token User's token
+     */
+    @Throws(ExpiredJwtException::class)
+    fun getClaims(token: String): Claims {
+        return Jwts.parser().setSigningKey(signatureKey).parseClaimsJws(token).body
+    }
+
+    /**
+     * Validate if the token is not expired
+     * @param token User's token
+     * @return True if the token is not expired, otherwise, false
+     */
+    fun validToken(token: String): Boolean {
+        try {
+            val claims: Claims = getClaims(token)
+            val date: LocalDateTime = claims.expiration.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
+
+            return !LocalDateTime.now().isAfter(date)
+        }catch (e: Exception){
+            return false
+        }
+    }
+
+    /**
+     * Extract the user email from token
+     * @param token User's token
+     */
+    @Throws(ExpiredJwtException::class)
+    fun getUserLogin(token: String): String {
+        return getClaims(token).subject
+    }
 }

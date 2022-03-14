d8a259b624dd631d2babc9ca06a1fcf824d6e49a
JoseBieco <jo_bieco@hotmail.com>
2022-03-14 09:07:06 -0300

    feat(auth/dto/ChangePassword): created dto to update just the password

--- src/main/kotlin/api/atomical/auth/dto/ChangePasswordDto.kt
+++ src/main/kotlin/api/atomical/auth/dto/ChangePasswordDto.kt
@@ -1,4 +1,12 @@
 package api.atomical.auth.dto
 
-class ChangePasswordDto {
-}
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+class ChangePasswordDto (
+    @field:NotBlank(message = "Password field cannot be blank")
+    @field:NotNull(message = "Password field cannot be null.")
+    @field:Size(min = 2, message = "The password size can't be lower than 2 characters.")
+    var password: String
+)

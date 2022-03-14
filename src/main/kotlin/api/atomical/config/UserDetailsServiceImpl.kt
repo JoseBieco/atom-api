b628a25a3f72cfad771538c9000dcbc068fa7236
JoseBieco <jo_bieco@hotmail.com>
2022-03-14 14:33:58 -0300

    feat(config/UserDetailsServiceImpl): implemented the UserDetails interface to configute web security

--- src/main/kotlin/api/atomical/config/UserDetailsServiceImpl.kt
+++ src/main/kotlin/api/atomical/config/UserDetailsServiceImpl.kt
@@ -1,4 +1,33 @@
 package api.atomical.config
 
-class UserDetailsServiceImpl {
+import api.atomical.user.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class UserDetailsServiceImpl(
+    @Autowired
+    val db: UserRepository
+): UserDetailsService {
+
+    override fun loadUserByUsername(login: String): UserDetails {
+        val user =  db.getByEmail(login)
+            .orElseThrow { UsernameNotFoundException("User not found!") }
+
+        /**
+         * TODO
+         */
+        val roles = arrayOf("USER")
+
+        return User
+            .builder()
+            .username(user.email)
+            .password(user.password)
+            .roles(*roles)
+            .build()
+    }
 }
